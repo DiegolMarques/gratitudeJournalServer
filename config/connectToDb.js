e6e18d59eb89001cab7bc7143a04902e7828d8cd
unknown <thediegomarques@gmail.com>
2023-01-02 00:39:16 -0500

    pushing to web

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -5,7 +5,8 @@ connectToDb().catch(err => console.log(err));
 
 async function connectToDb() {
     try {
-        await mongoose.connect(process.env.DB_URL);
+        //await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect('mongodb+srv://DiegoMarques:Awesome123*@cluster0.glogkbr.mongodb.net/?retryWrites=true&w=majority');
         console.log("Connected to database")
     } catch (err) {
         console.log(err)
